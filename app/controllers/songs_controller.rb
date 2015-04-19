01c22286a9709947696626c595cf986c36c26d6e
Gary L. Clark, II <gclark2@gmail.com>
2015-04-19 18:03:07 -0400

    Adds Song controller and updates Song related files

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,75 @@
+class SongsController < ApplicationController
+  def index
+    @songs  = Song.sorted_by_title
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    # Instantiate a new object
+    @song = Song.new(song_params)
+
+    # Save the object
+    if @song.save
+      # If save succeeds, redirect
+      # flash[:notice] = 'song created successfully'
+      # redirect_to(:action => 'index')
+    # else
+    #   # If save fails display the form
+    #   render('new')
+    end
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    # Find an existing object to use for its form parameters
+    @song = Song.find(params[:id])
+
+    # Update the object
+    if @song.update_attributes(song_params)
+      # If update succeeds, redirect
+    #   flash[:notice] = 'song "#{song.title}" updated successfully'
+    #   redirect_to(:action => 'show', :id => @song.id)
+    # else
+    #   # If update fails display the form
+    #   render('edit')
+    end
+  end
+
+  def delete
+    @song = Song.find(params[:id])
+  end
+
+  def destroy
+    song = Song.find(params[:id]).destroy
+    # flash[:notice] = 'song "#{song.title}" deleted successfully'
+    # redirect_to(:action => 'index')
+  end
+
+  private
+
+    def song_params
+      # Same as using "params[:song]", except that it:
+      # - raises an error if :song is not present
+      # - allows listed attributes to be mass-assigned
+      params.require(:song).permit(
+        :title,
+        :description,
+        :original_release_date,
+        :permalink,
+        :file_uri,
+        :genre_ids,
+        :theme_ids,
+        :tag_ids
+      )
+    end
+end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,15 @@
 class Song < ActiveRecord::Base
   belongs_to :album
   belongs_to :artist
+
   has_many :genres
+  accepts_nested_attributes_for :genres, allow_destroy: true
+
   has_many :tags
+  accepts_nested_attributes_for :tags
+
   has_many :themes
+  accepts_nested_attributes_for :themes
 
   scope :sorted_by_title, lambda { order("songs.title ASC") }
   scope :sorted_by_title_reverse, lambda { order("songs.title DESC") }
