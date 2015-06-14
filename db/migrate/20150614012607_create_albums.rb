9a358a1d8338856ab328fb2d7921b4135efb5f77
Gary L. Clark, II <gclark2@gmail.com>
2015-06-14 08:07:09 -0400

    Temp Seed Complete
    Updates seed data and migrations so they complete cleanly

--- db/migrate/20150614012607_create_albums.rb
+++ db/migrate/20150614012607_create_albums.rb
@@ -4,11 +4,11 @@ class CreateAlbums < ActiveRecord::Migration
       t.string :title
       t.text :description
       t.date :original_release_date
-      t.reference :artist
+      t.references :artist, index: true, foreign_key: true
 
       t.timestamps null: false
     end
     add_index :albums, :title, unique: true
-    add_index :albums, :artist
+    # add_index :albums, :artist_id
   end
 end
--- db/migrate/20150614012812_create_songs.rb
+++ db/migrate/20150614012812_create_songs.rb
@@ -5,11 +5,11 @@ class CreateSongs < ActiveRecord::Migration
       t.text :description
       t.date :original_release_date
       t.string :length
-      t.reference :album
+      t.references :album, index: true, foreign_key: true
 
       t.timestamps null: false
     end
     add_index :songs, :title, unique: true
-    add_index :songs, :album
+    # add_index :songs, :album
   end
 end
