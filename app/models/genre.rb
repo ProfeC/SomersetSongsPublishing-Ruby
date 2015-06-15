d03c3b7530c58929f652a26a00818ed9b27598fd
Gary L. Clark, II <gclark2@gmail.com>
2015-06-14 22:53:37 -0400

    Song Search
    Attempting to get the search working properly.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,8 @@
 class Genre < ActiveRecord::Base
-  has_and_belongs_to_many :songs
-end
+  has_and_belongs_to_many :songs, :join_table => 'genres_songs'
+
+  def self.search_by_song(id)
+    where("song.id ILIKE ?", "#{id}")
+  end
+
+end # end class
