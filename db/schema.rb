a8e1e13af66e981096241032133fe5c60a1836f8
Gary L. Clark, II <gclark2@gmail.com>
2015-06-20 20:44:43 -0400

    Scheme update
    I think this was missed in one of the last commits.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150614015918) do
+ActiveRecord::Schema.define(version: 20150620170832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,12 @@ ActiveRecord::Schema.define(version: 20150614015918) do
     t.text     "description"
     t.date     "original_release_date"
     t.integer  "artist_id"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "cover_art_file_name"
+    t.string   "cover_art_content_type"
+    t.integer  "cover_art_file_size"
+    t.datetime "cover_art_updated_at"
   end
 
   add_index "albums", ["artist_id"], name: "index_albums_on_artist_id", using: :btree
@@ -31,8 +35,12 @@ ActiveRecord::Schema.define(version: 20150614015918) do
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "cover_art_file_name"
+    t.string   "cover_art_content_type"
+    t.integer  "cover_art_file_size"
+    t.datetime "cover_art_updated_at"
   end
 
   add_index "artists", ["name"], name: "index_artists_on_name", unique: true, using: :btree
@@ -77,8 +85,16 @@ ActiveRecord::Schema.define(version: 20150614015918) do
     t.date     "original_release_date"
     t.string   "length"
     t.integer  "album_id"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "cover_art_file_name"
+    t.string   "cover_art_content_type"
+    t.integer  "cover_art_file_size"
+    t.datetime "cover_art_updated_at"
+    t.string   "audio_file_name"
+    t.string   "audio_content_type"
+    t.integer  "audio_file_size"
+    t.datetime "audio_updated_at"
   end
 
   add_index "songs", ["album_id"], name: "index_songs_on_album_id", using: :btree
@@ -100,6 +116,16 @@ ActiveRecord::Schema.define(version: 20150614015918) do
   end
 
   add_index "themes", ["title"], name: "index_themes_on_title", unique: true, using: :btree
+
+  create_table "vs_database_diagrams", id: false, force: :cascade do |t|
+    t.string   "name",     limit: 80
+    t.text     "diadata"
+    t.string   "comment",  limit: 1022
+    t.text     "preview"
+    t.string   "lockinfo", limit: 80
+    t.datetime "locktime"
+    t.string   "version",  limit: 80
+  end
 
   add_foreign_key "albums", "artists"
   add_foreign_key "songs", "albums"
