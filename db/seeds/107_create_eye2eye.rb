00c1d6d958301e5656a4c2ede6fa983648e6ac4b
Gary L. Clark, II <gclark2@gmail.com>
2015-08-12 21:34:44 -0400

    Removes Eye2Eye album

--- db/seeds/107_create_eye2eye.rb
+++ db/seeds/107_create_eye2eye.rb
@@ -8,65 +8,65 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-album = Album.create!(
-  artist_id: Artist.find_by(name: 'The Scorpions').id,
-  description: '',
-  original_release_date: '',
-  title: 'Eye II Eye'
-)
+# album = Album.create!(
+#   artist_id: Artist.find_by(name: 'The Scorpions').id,
+#   description: '',
+#   original_release_date: '',
+#   title: 'Eye II Eye'
+# )
 
-songs_list = [
-  {
-    album: album.title,
-    audio: '',
-    cover_art: '',
-    genre: ['rock', 'ballad'],
-    length: '00:03:52',
-    mood: ['futuristic'],
-    date: '1999-11-22',
-    theme: ['risk'],
-    title: '10 Light Years Away'
-  }
-]
+# songs_list = [
+#   {
+#     album: album.title,
+#     audio: '',
+#     cover_art: '',
+#     genre: ['rock', 'ballad'],
+#     length: '00:03:52',
+#     mood: ['futuristic'],
+#     date: '1999-11-22',
+#     theme: ['risk'],
+#     title: '10 Light Years Away'
+#   }
+# ]
 
-songs_list.each do |song|
+# songs_list.each do |song|
 
-  # Check for a specific release date. If there isn't one, then use the albums
-  if song[:date].present?
-    date = song[:date]
-  else
-    date = album.original_release_date
-  end
+#   # Check for a specific release date. If there isn't one, then use the albums
+#   if song[:date].present?
+#     date = song[:date]
+#   else
+#     date = album.original_release_date
+#   end
 
-  s = Song.create(
-    title: song[:title],
-    album_id: album.id,
-    audio: song[:audio],
-    cover_art: song[:conver_art],
-    description: song[:description],
-    length: song[:length],
-    original_release_date: date
-  )
+#   s = Song.create(
+#     title: song[:title],
+#     album_id: album.id,
+#     audio: song[:audio],
+#     cover_art: song[:conver_art],
+#     description: song[:description],
+#     length: song[:length],
+#     original_release_date: date
+#   )
 
-  if song[:genre].present?
-    song[:genre].each do |g|
-      genre = Genre.find_or_create_by(title: g)
-      s.genres << genre
-    end
-  end
+#   if song[:genre].present?
+#     song[:genre].each do |g|
+#       genre = Genre.find_or_create_by(title: g)
+#       s.genres << genre
+#     end
+#   end
 
-  if song[:mood].present?
-    song[:mood].each do |m|
-      mood = Mood.find_or_create_by(title: m)
-      s.moods << mood
-    end
-  end
+#   if song[:mood].present?
+#     song[:mood].each do |m|
+#       mood = Mood.find_or_create_by(title: m)
+#       s.moods << mood
+#     end
+#   end
 
-  if song[:theme].present?
-    song[:theme].each do |t|
-      theme = Theme.find_or_create_by(title: t)
-      s.themes << theme
-    end
-  end
+#   if song[:theme].present?
+#     song[:theme].each do |t|
+#       theme = Theme.find_or_create_by(title: t)
+#       s.themes << theme
+#     end
+#   end
 
-end
+# end
