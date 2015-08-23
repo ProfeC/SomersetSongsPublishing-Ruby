24dbce7dcabecacf5dbb3b6a35c1c7b93071579a
Gary L. Clark, II <gclark2@gmail.com>
2015-08-23 14:45:55 -0400

    Updates development mail delivery method.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
   config.action_mailer.smtp_settings = {
     address: ENV['mail_address'],
     port: ENV['mail_port'],
