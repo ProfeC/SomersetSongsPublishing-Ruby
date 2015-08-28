70b1644db68d5742ce68b003c7eb1c7782a2afda
Gary L. Clark, II <gclark2@gmail.com>
2015-08-28 15:06:24 -0400

    Song Form Updates
    - Updates flash message in controller.
    - Adds Font Awesome (via CDN) to layout.
    - Adds size restriction to uploaded files.
    - Adds icons to flash messages.
    - Cleans up form.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -185,7 +185,8 @@ class SongsController < ApplicationController
   def update
     respond_to do |format|
       if @song.update(song_params)
-        format.html { redirect_to @song, notice: 'Song was successfully updated.', style: 'success' }
+        flash[:success] = 'Song was successfully updated.'
+        format.html { redirect_to @song }
         format.json { render :show, status: :ok, location: @song }
       else
         format.html { render :edit }
