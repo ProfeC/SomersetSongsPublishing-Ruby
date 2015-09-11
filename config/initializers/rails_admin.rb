5839c461efcb0102d9cd88109db3345910e2db03
Gary L. Clark, II <gclark2@gmail.com>
2015-09-11 06:30:53 -0400

    Update Devise/Rails Admin Config
    - Update path for Devise methods
    - Add options for Rails Admin asset compilation
    - Update Rails Admin display title

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,9 @@
 RailsAdmin.config do |config|
 
+  # config.main_app_name = ["Cool app", "BackOffice"]
+  # or somethig more dynamic
+  config.main_app_name = Proc.new { |controller| [ "Somerset Songs Library", "BackOffice - #{controller.params[:action].try(:titleize)}" ] }
+
   ### Popular gems integration
 
   ## == Devise ==
