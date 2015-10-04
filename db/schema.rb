6a47302eb139dcb0c46b54003d7a773962600dee
Gary L. Clark, II <gclark2@gmail.com>
2015-10-04 00:44:15 -0400

    View Updates
    Adds Page data to views where needed.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004015015) do
+ActiveRecord::Schema.define(version: 20151004040741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,8 +108,10 @@ ActiveRecord::Schema.define(version: 20151004015015) do
     t.string   "title"
     t.text     "body"
     t.string   "slug"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "subtitle"
+    t.string   "leading_text"
   end
 
   create_table "songs", force: :cascade do |t|
