e9e5fd145e458820a7969b45bc0dbd63888a27b6
Gary L. Clark, II <gclark2@gmail.com>
2015-10-13 01:14:20 -0400

    Removes timeout config.

--- config/initializers/timeout.rb
+++
@@ -1,4 +0,0 @@
-# Reference: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#timeout
-
-# config/initializers/timeout.rb
-Rack::Timeout.timeout = 20  # seconds
